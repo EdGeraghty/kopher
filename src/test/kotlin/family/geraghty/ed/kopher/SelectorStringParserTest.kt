ee7d69f27af2f23e5ee1153486dbb8bd34849e1f
Ed Geraghty <ed@geraghty.family>
2023-12-02 15:37:51 +0000

    Clean up test comment to better reflect what I'm testing

--- src/test/kotlin/family/geraghty/ed/kopher/SelectorStringParserTest.kt
+++ src/test/kotlin/family/geraghty/ed/kopher/SelectorStringParserTest.kt
@@ -9,26 +9,19 @@ class SelectorStringParserTest {
      * Test adapted from https://datatracker.ietf.org/doc/html/rfc1436#section-2
      *
      *    Below is a simple example of a client/server interaction; more
-     *    complex interactions are dealt with later.  Assume that a "well-
-     *    known" Gopher server (this may be duplicated, details are discussed
-     *    later) listens at a well known port for the campus (much like a
-     *    domain-name server).
+     *    complex interactions are dealt with later.
+     *
      *    [...]
+     *    Client: <CR><LF> {Sends an empty line: Meaning "list what you have"}
      *
-     *  Client:          {Opens connection to Server}
-     *
-     *  Server:          {Accepts connection but says nothing}
-     *
-     *  Client: <CR><LF> {Sends an empty line: Meaning "list what you have"}
-     *
-     *  Server:          {Sends a series of lines, each ending with CR LF}
-     *  0About internet Gopher  Stuff:About us  rawBits.micro.umn.edu   70
-     *  1Around University of Minnesota Z,5692,AUM  underdog.micro.umn.edu   70
-     *  1Microcomputer News & Prices    Prices/    pserver.bookstore.umn.edu  70
-     *  1Courses, Schedules, Calendars      events.ais.umn.edu  9120
-     *  1Student-Staff Directories      uinfo.ais.umn.edu   70
-     *  1Departmental Publications  Stuff:DP:   rawBits.micro.umn.edu   70
-     *  .                  {Period on a line by itself}
+     *    Server: {Sends a series of lines, each ending with CR LF}
+     *    0About internet Gopher  Stuff:About us  rawBits.micro.umn.edu   70
+     *    1Around University of Minnesota Z,5692,AUM  underdog.micro.umn.edu   70
+     *    1Microcomputer News & Prices    Prices/    pserver.bookstore.umn.edu  70
+     *    1Courses, Schedules, Calendars      events.ais.umn.edu  9120
+     *    1Student-Staff Directories      uinfo.ais.umn.edu   70
+     *    1Departmental Publications  Stuff:DP:   rawBits.micro.umn.edu   70
+     *    . {Period on a line by itself}
      */
     @Test
     fun `Sends an empty line meaning list what you have`() {
