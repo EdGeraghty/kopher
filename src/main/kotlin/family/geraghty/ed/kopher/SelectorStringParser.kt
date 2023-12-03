7d31e4e3110853db6748d5ed5736b5df7a7cc7b1
Ed Geraghty <ed@geraghty.family>
2023-12-03 20:21:43 +0000

    Implement listing what we have

--- src/main/kotlin/family/geraghty/ed/kopher/SelectorStringParser.kt
+++ src/main/kotlin/family/geraghty/ed/kopher/SelectorStringParser.kt
@@ -1,5 +1,9 @@
 package family.geraghty.ed.kopher
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import java.io.File
+
 class SelectorStringParser (private var baseDir: String) {
     fun parse(selectorString: String) : String {
         return try {
@@ -15,6 +19,33 @@ class SelectorStringParser (private var baseDir: String) {
             throw Exception("The Selector string should be no longer than 255 characters.")
         }
 
+        if (!File(baseDir).isDirectory()) {
+            throw Exception("Server error.")
+        }
+
+        if(!File(baseDir).canRead()) {
+            throw Exception("Server error.")
+        }
+
+        if (selectorString == "\r\n") {
+            return listWhatYouHave(File("$baseDir/DirectoryListing.json").readText())
+        }
         throw Exception("To be implemented")
+    }
+
+    private fun listWhatYouHave(directoryListing: String): String {
+        val mapper = jacksonObjectMapper()
+        val dirEntitiesList: List<DirEntity> = mapper.readValue(directoryListing)
+
+        var returnVar = ""
+
+        for (dirEntity in dirEntitiesList) {
+            returnVar += dirEntity.toString()
+            returnVar += "\r\n"
+        }
+
+        returnVar += "."
+
+        return returnVar
     }
 }
