eb98119ee8d750b886bded257a812c0821422b78
Ed Geraghty <ed@geraghty.family>
2023-12-16 17:01:42 +0000

    Remove empty primary constructor

--- src/test/kotlin/family/geraghty/ed/kopher/SelectorStringParserTest.kt
+++ src/test/kotlin/family/geraghty/ed/kopher/SelectorStringParserTest.kt
@@ -3,7 +3,7 @@ package family.geraghty.ed.kopher
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-class SelectorStringParserTest() {
+class SelectorStringParserTest {
     private val directoryListingJson = """
         [
           {
