c0a2c27cab2e89d5336512c378ea9f5e25dd0885
Ed Geraghty <ed@geraghty.family>
2023-12-30 18:44:51 +0000

    Change `itemType` to an ENUM
    Allows us to clean up our main logic

--- src/main/kotlin/family/geraghty/ed/kopher/DirEntity.kt
+++ src/main/kotlin/family/geraghty/ed/kopher/DirEntity.kt
@@ -1,7 +1,31 @@
 package family.geraghty.ed.kopher
 
+/**
+ * Taken from https://datatracker.ietf.org/doc/html/rfc1436#section-3.8
+ */
+enum class ItemType(private val itemType: Char) {
+    FILE('0'),
+    DIRECTORY('1'),
+    CSO_BOOK_SERVER('2'),
+    ERROR('3'), // We don't actually use this, just for completeness
+    BINHEXED_MACINTOSH_FILE('4'),
+    DOS_BINARY_ARCHIVE('5'),
+    UNIX_UUENCODED_FILE('6'),
+    INDEX_SEARCH_SERVER('7'),
+    TEXT_BASED_TELNET_SESSION('8'),
+    BINARY_FILE('9'),
+    REDUNDANT_SERVER('+'),
+    TEXT_BASED_TN3270_SESSION('T'),
+    GIF_FILE('g'),
+    IMAGE('I');
+
+    override fun toString(): String {
+        return itemType.toString()
+    }
+}
+
 data class DirEntity(
-    val itemType: Char,
+    val itemType: ItemType,
     val userName: String,
     var selectorString: String,
     var host: String,
--- src/main/kotlin/family/geraghty/ed/kopher/SelectorStringParser.kt
+++ src/main/kotlin/family/geraghty/ed/kopher/SelectorStringParser.kt
@@ -6,12 +6,17 @@ import java.io.File
 import java.io.FileReader
 
 class SelectorStringParser(private val baseDirectory: String, private val directoryListingJson: String) {
+
+    /**
+     * Parse the [selectorString] and output the result.
+     *
+     * If an exception is thrown, output with the correct `ItemType`
+     */
     fun parse(selectorString: String): String {
         return try {
             parseWithThrows(selectorString)
         } catch (e: Exception) {
-            "3${e.message}\r\n" +
-                "."
+            "${ItemType.ERROR}${e.message}\r\n."
         }
     }
 
@@ -20,28 +25,38 @@ class SelectorStringParser(private val baseDirectory: String, private val direct
             throw Exception("The Selector string should be no longer than 255 characters.")
         }
 
-        var output = ""
-
         if (selectorString == "\r\n") {
-            output = listWhatYouHave()
+            return listWhatYouHave()
         } else {
             val selector = selectorString.substringBefore("\t")
             val dirEntity = deserializeToDirEntities().first { it.selectorString == selector }
 
-            when (dirEntity.itemType) {
-                '0' -> output = outputTextFile(dirEntity) + "." // output ends with a new line
-                '9', '5' -> output = outputBinaryFile(dirEntity) // No trailing '.'
+            return when (dirEntity.itemType) {
+                ItemType.FILE -> outputTextFile(dirEntity)
+                ItemType.DOS_BINARY_ARCHIVE, ItemType.BINARY_FILE -> outputBinaryFile(dirEntity)
+                ItemType.DIRECTORY -> TODO()
+                ItemType.BINHEXED_MACINTOSH_FILE -> TODO()
+                ItemType.UNIX_UUENCODED_FILE -> TODO()
+                ItemType.TEXT_BASED_TELNET_SESSION -> TODO()
+                ItemType.TEXT_BASED_TN3270_SESSION -> TODO()
+                ItemType.GIF_FILE -> TODO()
+                ItemType.IMAGE -> TODO()
+                // Don't think I'll ever implement these
+                ItemType.CSO_BOOK_SERVER, ItemType.INDEX_SEARCH_SERVER, ItemType.REDUNDANT_SERVER -> TODO()
+                // Only here for completeness
+                ItemType.ERROR -> TODO()
             }
         }
-
-        if (output.isNotEmpty()) {
-            return output
-        }
-
-        throw Exception("To be implemented")
     }
 
+    /**
+     *
+     */
     private fun outputTextFile(dirEntity: DirEntity): String {
+        if (dirEntity.itemType != ItemType.FILE) { // In theory, we should never get here
+            throw Exception("$dirEntity is not a text file")
+        }
+
         var returnVal = ""
 
         FileReader(baseDirectory + dirEntity.realPath!!)
@@ -52,7 +67,7 @@ class SelectorStringParser(private val baseDirectory: String, private val direct
                 returnVal += "${it}\r\n"
             }
 
-        return returnVal
+        return "$returnVal."
     }
 
     private fun outputBinaryFile(dirEntity: DirEntity): String {
